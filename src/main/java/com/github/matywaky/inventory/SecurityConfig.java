e2f9cfcf3d0b15a023c15a352d55d48a45c37032
Mateusz Nowak <mateusz00nowak@gmail.com>
2024-10-01 21:43:24 +0200

    First commit

---
+++ src/main/java/com/github/matywaky/inventory/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.github.matywaky.inventory;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .anyRequest().permitAll() // Zezwala na dostęp do wszystkich żądań
+                )
+                .csrf(csrf -> csrf.disable()); // Możesz wyłączyć CSRF, jeśli to potrzebne
+
+        return http.build();
+    }
+}
