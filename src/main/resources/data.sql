eb5e53b309993ae84ea382e215ad52f20431d58e
Mateusz Nowak <mateusz00nowak@gmail.com>
2024-11-21 18:00:19 +0100

    New OS version

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,17 @@
+-- employees
 INSERT INTO employees(name, surname) VALUES ('Admin', 'Adminowsky');
-INSERT INTO roles(description, name) VALUES ('Full access', 'admin');
+INSERT INTO employees(name, surname) VALUES ('Test', 'Testowy');
 
+-- roles
+INSERT INTO roles(description, name) VALUES ('Full access', 'admin');
+INSERT INTO roles(description, name) VALUES ('Employee access', 'employee');
+
+-- users
 INSERT INTO
     users(email, password, employee_id, role_id)
 VALUES
-    ('admin@admin.admin', '$2a$10$wXsq1mRlX1ySkpqyEDUpNDozv/5r5tS/DezJOfYZizg5g9vJz9sZ2', 1, 1);
+    ('admin@admin.admin', '{noop}admin', 1, 1);
+INSERT INTO
+    users(email, password, employee_id, role_id)
+VALUES
+    ('test@test.test', '{noop}test', 2, 2);
